e89c0018302d55c84e41aee3c00107c2b907e1ce
REDcat <www.324132.com@gmail.com>
2022-12-26 02:26:46 +0500

    shrink

---
+++ DedicatedServer/Logger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace DedicatedServer
+{
+    internal class Logger
+    {
+        
+        public void Log(string message)
+        {
+            string time = DateTime.Now.ToString() + '.' + DateTime.Now.Millisecond.ToString();
+            if (File.Exists("log.txt"))
+            {
+                File.AppendAllText("log.txt", $"[{time}] " + message + Environment.NewLine);
+            }
+            else
+            {
+                File.Create("log.txt");
+                File.AppendAllText("log.txt", $"[{time}] " + message + Environment.NewLine);
+            }
+        }
+    }
+}
--- DedicatedServer/MyMod.cs
+++ DedicatedServer/MyMod.cs
@@ -21,16 +21,12 @@ namespace SkyCoop
         }
         public static int MaxPlayers = 2;
         public static bool DedicatedServerAppMode = true;
-        public static List<DataStr.MultiPlayerClientData> playersData = new List<DataStr.MultiPlayerClientData>();
+
         public static int MinutesFromStartServer = 0;
-        public static List<DataStr.BrokenFurnitureSync> BrokenFurniture = new List<DataStr.BrokenFurnitureSync>();
-        public static List<DataStr.PickedGearSync> PickedGears = new List<DataStr.PickedGearSync>();
-        public static List<DataStr.ClimbingRopeSync> DeployedRopes = new List<DataStr.ClimbingRopeSync>();
-        public static DataStr.ServerConfigData ServerConfig = new DataStr.ServerConfigData();
-        public static List<DataStr.ContainerOpenSync> LootedContainers = new List<DataStr.ContainerOpenSync>();
-        public static List<string> HarvestedPlants = new List<string>();
-        public static List<DataStr.ShowShelterByOther> ShowSheltersBuilded = new List<DataStr.ShowShelterByOther>();
-        public static Dictionary<int, bool> FoundCairns = new Dictionary<int, bool>();
+
+
+
+
         public static bool iAmHost = true;
         public static int OverridedMinutes = 0;
         public static int OverridedHourse = 12;
@@ -44,24 +40,37 @@ namespace SkyCoop
         public static int TimeOutSeconds = 300;
         public static int TimeOutSecondsForLoaders = 300;
         public static int PlayersOnServer = 0;
-        public static Dictionary<string, bool> OpenableThings = new Dictionary<string, bool>();
-        public static Dictionary<int, string> SlicedJsonDataBuffer = new Dictionary<int, string>();
+
         public static string level_guid = "";
-        public static List<DataStr.PickedGearSync> RecentlyPickedGears = new List<DataStr.PickedGearSync>();
-        public static Dictionary<string, int> BannedSpawnRegions = new Dictionary<string, int>();
+
         public static string NotificationString = "";
         public static bool DebugTrafficCheck = false;
         public static string RCON = "12345";
-        public static DataStr.CustomChallengeData CurrentCustomChalleng = new DataStr.CustomChallengeData();
-        public static DataStr.CustomChallengeRules CurrentChallengeRules = new DataStr.CustomChallengeRules();
+        
         public static bool PVP = false;
         public static int NoHostResponceSeconds = 0;
-        public static List<DataStr.DeathContainerData> DeathCreates = new List<DataStr.DeathContainerData>();
+        
         public static int RestartPerioud = -1;
         public static int SecondsWithoutSaving = 0;
         public static int DsSavePerioud = 60;
         public static string SavedSceneForSpawn = "";
         public static System.Numerics.Vector3 SavedPositionForSpawn = System.Numerics.Vector3.Zero;
+        public static DataStr.CustomChallengeData CurrentCustomChalleng = new DataStr.CustomChallengeData();
+        public static DataStr.CustomChallengeRules CurrentChallengeRules = new DataStr.CustomChallengeRules();
+        public static DataStr.ServerConfigData ServerConfig = new DataStr.ServerConfigData();
+        public static List<DataStr.DeathContainerData> DeathCreates = new List<DataStr.DeathContainerData>();
+        public static List<DataStr.BrokenFurnitureSync> BrokenFurniture = new List<DataStr.BrokenFurnitureSync>();
+        public static List<DataStr.PickedGearSync> PickedGears = new List<DataStr.PickedGearSync>();
+        public static List<DataStr.ClimbingRopeSync> DeployedRopes = new List<DataStr.ClimbingRopeSync>();
+        public static List<DataStr.ContainerOpenSync> LootedContainers = new List<DataStr.ContainerOpenSync>();
+        public static List<string> HarvestedPlants = new List<string>();
+        public static List<DataStr.ShowShelterByOther> ShowSheltersBuilded = new List<DataStr.ShowShelterByOther>();
+        public static List<DataStr.MultiPlayerClientData> playersData = new List<DataStr.MultiPlayerClientData>();
+        public static List<DataStr.PickedGearSync> RecentlyPickedGears = new List<DataStr.PickedGearSync>();
+        public static Dictionary<string, int> BannedSpawnRegions = new Dictionary<string, int>();
+        public static Dictionary<int, bool> FoundCairns = new Dictionary<int, bool>();
+        public static Dictionary<string, bool> OpenableThings = new Dictionary<string, bool>();
+        public static Dictionary<int, string> SlicedJsonDataBuffer = new Dictionary<int, string>();
 
         static float CurrentTime0 = 0f;
         static float CurrentTime1 = 0f;
@@ -88,6 +97,7 @@ namespace SkyCoop
         {
 
             ResourceIndependent.Init();
+            Shared.HostAServer();
             base.Initialize();
         }
 
