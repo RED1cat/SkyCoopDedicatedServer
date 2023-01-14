1111411099b98a667605131f14a1da945f5f640b
REDcat <www.324132.com@gmail.com>
2023-01-14 18:59:45 +0500

    minor update

--- DedicatedServer/Program.cs
+++ DedicatedServer/Program.cs
@@ -1,3 +1,26 @@
-﻿
+﻿#if (DEDICATED_LINUX)
+using System;
+namespace DedicatedServer
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+
+            if (arg.Length > 0)
+            {
+
+            }
+            else
+            {
+                using var game = new SkyCoop.MyMod();
+                game.Run();
+            }
+        }
+    }
+}
+#endif
+#if(DEDICATED_WINDOWS)
 using var game = new SkyCoop.MyMod();
 game.Run();
+#endif
