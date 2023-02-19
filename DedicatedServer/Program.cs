b4278350ccba005bc5b5038ba0336c4c8e6b7693
Filigrani <filigranisb@gmail.com>
2023-02-19 20:20:45 +0400

    ForceXNA on WINDOWS_DEBUG

--- DedicatedServer/Program.cs
+++ DedicatedServer/Program.cs
@@ -3,19 +3,31 @@
     class Program
     {
         public static bool NoGraphics;
-        public static XnaMain xnaMain = new XnaMain(); 
+        public static XnaMain xnaMain = new XnaMain();
+#if (WINDOWS_DEBUG)
+        static bool ForceXNA = true;
+#else
+        static bool ForceXNA = false;
+#endif
+
         public static void Main(string[] arg)
         {
-            if(arg.Length == 0)
-            {
-                NoGraphics = true;
-                ConsoleMain serv = new ConsoleMain();
-                serv.Initialize();
-            }
-            else
+            if (ForceXNA)
             {
                 NoGraphics = false;
                 xnaMain.Run();
+            } else
+            {
+                if (arg.Length == 0)
+                {
+                    NoGraphics = true;
+                    ConsoleMain serv = new ConsoleMain();
+                    serv.Initialize();
+                } else
+                {
+                    NoGraphics = false;
+                    xnaMain.Run();
+                }
             }
         }
     }
